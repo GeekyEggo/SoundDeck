5808ef6f9c75296bbd782dcb1e577e06e1934b82
GeekyEggo <richard.herman@gmail.com>
2019-08-15 16:24:19 +0100

    Added distribution scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,86 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin")
+const package = require("./package.json");
 
-const source = path.resolve(__dirname, "src/SoundDeck.PI");
-const dest = path.resolve(process.env["APPDATA"], "Elgato/StreamDeck/Plugins/com.geekyeggo.sounddeck.sdPlugin/PI");
+module.exports = (env, argv) => {
+    const source = path.resolve(__dirname, package.paths.pi);
+    const dest = argv.dist
+        ? path.resolve(__dirname, package.paths.dist, "PI")
+        : path.resolve(process.env["APPDATA"], package.paths.elgato, "PI");
 
-let config = {
-    entry: {
-        index: path.resolve(source, "js/index.jsx")
-    },
-    module: {
-        rules: [{
-            test: /\.css$/,
-            use: [
-                { loader: 'style-loader/url' },
-                {
-                    loader: 'file-loader',
-                    options: {
-                        name: "./css/[name].css"
-                    }
-                }],
+    let config = {
+        entry: {
+            index: path.resolve(source, "js/index.jsx")
         },
-        {
-            test: /\.jsx$/,
-            include: [
-                /react-sharpdeck/,
-                source
-            ],
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader",
-                options: {
-                    presets: ["@babel/preset-react"]
+        module: {
+            rules: [{
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader/url' },
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: "./css/[name].css"
+                        }
+                    }],
+            },
+            {
+                test: /\.jsx$/,
+                include: [
+                    /react-sharpdeck/,
+                    source
+                ],
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-react"]
+                    }
                 }
-            }
-        }]
-    },
-    output: {
-        chunkFilename: "./js/[name].bundle.js",
-        filename: "./js/[name].js",
-        path: dest
-    },
-    plugins: [
-        new CleanWebpackPlugin({
-            cleanAfterEveryBuildPatterns: ["!*.html", "!css/**/*.*", "!imgs/**/*.*"],
-        }),
-        new CopyPlugin([{
-            from: source,
-            to: dest,
-            context: source,
-            ignore: [".babelrc", "*.csproj", "*.js", "*.jsx", "bin/**/*.*", "obj/**/*.*", "js/react-sharpdeck/**/*"]
-        }])
-    ],
-    resolve: {
-        extensions: [".js", ".jsx"]
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                reactSharpdeck: {
-                    test: /react-sharpdeck/,
-                    chunks: 'all',
-                    name: "react-sharpdeck",
-                    enforce: true
-                },
-                vendor: {
-                    test: /node_modules/,
-                    chunks: "all",
-                    name: "vendor",
-                    enforce: true,
-                    priority: -1
+            }]
+        },
+        output: {
+            chunkFilename: "./js/[name].bundle.js",
+            filename: "./js/[name].js",
+            path: dest
+        },
+        plugins: [
+            new CleanWebpackPlugin({
+                cleanAfterEveryBuildPatterns: ["!*.html", "!css/**/*.*", "!imgs/**/*.*"],
+            }),
+            new CopyPlugin([{
+                from: source,
+                to: dest,
+                context: source,
+                ignore: [".babelrc", "*.csproj", "*.js", "*.jsx", "bin/**/*.*", "obj/**/*.*", "js/react-sharpdeck/**/*"]
+            }])
+        ],
+        resolve: {
+            extensions: [".js", ".jsx"]
+        },
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    reactSharpdeck: {
+                        test: /react-sharpdeck/,
+                        chunks: 'all',
+                        name: "react-sharpdeck",
+                        enforce: true
+                    },
+                    vendor: {
+                        test: /node_modules/,
+                        chunks: "all",
+                        name: "vendor",
+                        enforce: true,
+                        priority: -1
+                    }
                 }
             }
         }
-    }
-};
+    };
 
-module.exports = (env, argv) => {
+    // set the dev tool
     if (argv.mode === "development") {
         config.devtool = "inline-source-map";
     }
