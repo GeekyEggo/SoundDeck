d3afbb8e69eb71f1e9b4a581d113b265896bd253
GeekyEggo <richard.herman@gmail.com>
2021-03-07 17:08:25 +0000

    #28 set default device for Set App Audio Device

--- src/SoundDeck.PI/js/components/setAppAudioDeviceSettings.jsx
+++ src/SoundDeck.PI/js/components/setAppAudioDeviceSettings.jsx
@@ -22,7 +22,7 @@ class SetAppAudioDeviceSettings extends React.Component {
     render() {
         return (
             <PropertyInspectorWrapper>
-                <Select label="Audio Device" dataSourceUri="GetAppAssignableAudioDevices" valuePath="audioDeviceId" />
+                <Select label="Audio Device" dataSourceUri="GetAppAssignableAudioDevices" valuePath="audioDeviceId" defaultValue="PLAYBACK_DEFAULT" />
                 <Select label="Process" options={this.processSelectionOptions} valuePath="processSelectionType" />
                 {this.state.isProcessNameVisible &&
                     <TextField label="Name" valuePath="processName" />
